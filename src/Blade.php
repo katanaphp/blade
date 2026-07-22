1c6ce26e75d0f2eb4abc7f004b4a6602f2a9e41f
soysudhanshu <69431877+soysudhanshu@users.noreply.github.com>
2026-07-22 21:03:07 +0530

    Fixed: Type error

--- src/Blade.php
+++ src/Blade.php
@@ -138,8 +138,10 @@ final class Blade
     public function compile(string | Component $view): string
     {
         if (!$this->viewExists($view)) {
+            $componentName = is_string($view) ? $view : implode('::', array_filter([$view->namespace, $view->name]));
+
             throw new BladeException(
-                sprintf(Messages::ERROR_VIEW_NOT_FOUND, $view)
+                sprintf(Messages::ERROR_VIEW_NOT_FOUND, $componentName)
             );
         }
 
